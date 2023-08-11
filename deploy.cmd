8ed3e7bce4120b8002f8376ce4486522031da7ef
Shams Kitz <dustractor@gmail.com>
2023-08-10 23:34:27 -0500

    shave command
    its for a certain midi plugin that doesn't re-trigger when notes are touching and behaves better when they are a few pulses behind the beat

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,8 @@
 @echo off
-set ARTIFACT="Select Every Nth.pyscript"
+
 set TARGET="%USERPROFILE%\Documents\Image-Line\FL Studio\Settings\Piano roll scripts\"
-echo Copying:
-echo %ARTIFACT%
-echo to:
-echo %TARGET%
-copy /-y %ARTIFACT% %TARGET%
-echo OK
+
+echo Copying to: %TARGET%
+
+copy /-y *.pyscript %TARGET%
 
